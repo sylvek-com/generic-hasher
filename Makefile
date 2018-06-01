615967da70f6d4806807f489eb115ad4be072ff5
Sylvester Ziolkowski <sylvek@sylvek.com>
2018-06-01 15:56:08 -0700

    Added MD4 implementation and original documentation

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC=gcc
 CFLAGS=-Wall -g
 
-TGT=hasher basher washer
+TGT=hasher basher washer masher
 
 all:	$(TGT)
 
---
+++ masher.c
@@ -0,0 +1,292 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STEP 0
+#define BYTE unsigned char
+#define WORD unsigned int
+#define SIZE unsigned long long
+#define SW sizeof(WORD)
+#define SS sizeof(SIZE)
+#define NI 16
+#define NO 4
+#define NS 16
+#define SHA0 0
+
+static WORD accu[NO];
+
+static SIZE size;
+
+static WORD iv[NO] = {
+0x67452301,
+0xefcdab89,
+0x98badcfe,
+0x10325476,
+};
+
+#define BA(w) ((BYTE*)(w))
+
+static void print(const WORD wa[],int nw,const char sa[])
+{
+	int i,j;
+
+	for (i = 0; i < nw; ++i) {
+#if BYTE_ORDER == BIG_ENDIAN
+		for (j = SW-1; j >= 0; --j)
+#elif BYTE_ORDER == LITTLE_ENDIAN
+		for (j = 0; j <= SW-1; ++j)
+#else
+		for (;;)
+#endif
+			printf("%02x",BA(wa)[i*SW+j]);
+#if STEP
+		putc(' ',stdout);
+#endif
+	}
+	printf(" *%s\n",sa);
+}
+
+static void init(void)
+{
+	int i;
+
+	for (i = 0; i < NO; ++i)
+		accu[i] = iv[i];
+	size = 0ull;
+}
+
+#define ROTL(w,s) ((w) << (s) | (w) >> (32-s))
+#define ROTR(w,s) ((w) >> (s) | (w) << (32-s))
+
+static void next(const BYTE ba[NI*SW])
+{
+	int i,j;
+	WORD W[NI];
+	WORD H[NO];
+
+	for (i = 0; i < NI; ++i)
+#if BYTE_ORDER == BIG_ENDIAN
+		for (j = SW-1; j >= 0; --j)
+#elif BYTE_ORDER == LITTLE_ENDIAN
+		for (j = 0; j <= SW-1; ++j)
+#else
+		for (;;)
+#endif
+			BA(W)[i*SW+j] = *ba++;
+#if STEP
+	print(W,NI,"input");
+#endif
+	for (i = 0; i < NO; ++i)
+		H[i] = accu[i];
+#define a H[0]
+#define b H[1]
+#define c H[2]
+#define d H[3]
+#define x W
+#if 0
+        /* Round 1. */
+        /* Let [abcd k s] denote the operation
+             a = (a + F(b,c,d) + X[k]) <<< s. */
+        /* Do the following 16 operations. */
+        [ABCD  0  3]  [DABC  1  7]  [CDAB  2 11]  [BCDA  3 19]
+        [ABCD  4  3]  [DABC  5  7]  [CDAB  6 11]  [BCDA  7 19]
+        [ABCD  8  3]  [DABC  9  7]  [CDAB 10 11]  [BCDA 11 19]
+        [ABCD 12  3]  [DABC 13  7]  [CDAB 14 11]  [BCDA 15 19]
+
+        /* Round 2. */
+        /* Let [abcd k s] denote the operation
+             a = (a + G(b,c,d) + X[k] + 5A827999) <<< s. */
+        /* Do the following 16 operations. */
+        [ABCD  0  3]  [DABC  4  5]  [CDAB  8  9]  [BCDA 12 13]
+        [ABCD  1  3]  [DABC  5  5]  [CDAB  9  9]  [BCDA 13 13]
+        [ABCD  2  3]  [DABC  6  5]  [CDAB 10  9]  [BCDA 14 13]
+        [ABCD  3  3]  [DABC  7  5]  [CDAB 11  9]  [BCDA 15 13]
+
+        /* Round 3. */
+        /* Let [abcd k s] denote the operation
+             a = (a + H(b,c,d) + X[k] + 6ED9EBA1) <<< s. */
+        /* Do the following 16 operations. */
+        [ABCD  0  3]  [DABC  8  9]  [CDAB  4 11]  [BCDA 12 15]
+        [ABCD  2  3]  [DABC 10  9]  [CDAB  6 11]  [BCDA 14 15]
+        [ABCD  1  3]  [DABC  9  9]  [CDAB  5 11]  [BCDA 13 15]
+        [ABCD  3  3]  [DABC 11  9]  [CDAB  7 11]  [BCDA 15 15]
+
+#else
+
+/*
+ */ 
+#define S11 3
+#define S12 7
+#define S13 11
+#define S14 19
+#define S21 3
+#define S22 5
+#define S23 9
+#define S24 13
+#define S31 3
+#define S32 9
+#define S33 11
+#define S34 15
+
+/* F, G and H are basic MD4 functions.
+ */
+#define F(x, y, z) (((x) & (y)) | ((~x) & (z)))
+#define G(x, y, z) (((x) & (y)) | ((x) & (z)) | ((y) & (z)))
+#define H(x, y, z) ((x) ^ (y) ^ (z))
+
+/* ROTATE_LEFT rotates x left n bits.
+ */
+#define ROTATE_LEFT(x, n) (((x) << (n)) | ((x) >> (32-(n))))
+
+/* FF, GG and HH are transformations for rounds 1, 2 and 3 */
+/* Rotation is separate from addition to prevent recomputation */
+
+#define FF(a, b, c, d, x, s) { \
+    (a) += F ((b), (c), (d)) + (x); \
+    (a) = ROTATE_LEFT ((a), (s)); \
+  }
+#define GG(a, b, c, d, x, s) { \
+    (a) += G ((b), (c), (d)) + (x) + 0x5a827999u; \
+    (a) = ROTATE_LEFT ((a), (s)); \
+  }
+#define HH(a, b, c, d, x, s) { \
+    (a) += H ((b), (c), (d)) + (x) + 0x6ed9eba1u; \
+    (a) = ROTATE_LEFT ((a), (s)); \
+  }
+
+  /* Round 1 */
+  FF (a, b, c, d, x[ 0], S11); /* 1 */
+  FF (d, a, b, c, x[ 1], S12); /* 2 */
+  FF (c, d, a, b, x[ 2], S13); /* 3 */
+  FF (b, c, d, a, x[ 3], S14); /* 4 */
+  FF (a, b, c, d, x[ 4], S11); /* 5 */
+  FF (d, a, b, c, x[ 5], S12); /* 6 */
+  FF (c, d, a, b, x[ 6], S13); /* 7 */
+  FF (b, c, d, a, x[ 7], S14); /* 8 */
+  FF (a, b, c, d, x[ 8], S11); /* 9 */
+  FF (d, a, b, c, x[ 9], S12); /* 10 */
+  FF (c, d, a, b, x[10], S13); /* 11 */
+  FF (b, c, d, a, x[11], S14); /* 12 */
+  FF (a, b, c, d, x[12], S11); /* 13 */
+  FF (d, a, b, c, x[13], S12); /* 14 */
+  FF (c, d, a, b, x[14], S13); /* 15 */
+  FF (b, c, d, a, x[15], S14); /* 16 */
+
+  /* Round 2 */
+  GG (a, b, c, d, x[ 0], S21); /* 17 */
+  GG (d, a, b, c, x[ 4], S22); /* 18 */
+  GG (c, d, a, b, x[ 8], S23); /* 19 */
+  GG (b, c, d, a, x[12], S24); /* 20 */
+  GG (a, b, c, d, x[ 1], S21); /* 21 */
+  GG (d, a, b, c, x[ 5], S22); /* 22 */
+  GG (c, d, a, b, x[ 9], S23); /* 23 */
+  GG (b, c, d, a, x[13], S24); /* 24 */
+  GG (a, b, c, d, x[ 2], S21); /* 25 */
+  GG (d, a, b, c, x[ 6], S22); /* 26 */
+  GG (c, d, a, b, x[10], S23); /* 27 */
+  GG (b, c, d, a, x[14], S24); /* 28 */
+  GG (a, b, c, d, x[ 3], S21); /* 29 */
+  GG (d, a, b, c, x[ 7], S22); /* 30 */
+  GG (c, d, a, b, x[11], S23); /* 31 */
+  GG (b, c, d, a, x[15], S24); /* 32 */
+
+  /* Round 3 */
+  HH (a, b, c, d, x[ 0], S31); /* 33 */
+  HH (d, a, b, c, x[ 8], S32); /* 34 */
+  HH (c, d, a, b, x[ 4], S33); /* 35 */
+  HH (b, c, d, a, x[12], S34); /* 36 */
+  HH (a, b, c, d, x[ 2], S31); /* 37 */
+  HH (d, a, b, c, x[10], S32); /* 38 */
+  HH (c, d, a, b, x[ 6], S33); /* 39 */
+  HH (b, c, d, a, x[14], S34); /* 40 */
+  HH (a, b, c, d, x[ 1], S31); /* 41 */
+  HH (d, a, b, c, x[ 9], S32); /* 42 */
+  HH (c, d, a, b, x[ 5], S33); /* 43 */
+  HH (b, c, d, a, x[13], S34); /* 44 */
+  HH (a, b, c, d, x[ 3], S31); /* 45 */
+  HH (d, a, b, c, x[11], S32); /* 46 */
+  HH (c, d, a, b, x[ 7], S33); /* 47 */
+  HH (b, c, d, a, x[15], S34); /* 48 */
+
+#endif
+
+#undef a
+#undef b
+#undef c
+#undef d
+#undef x
+
+#if STEP
+		print(H,NO,"round");
+#endif
+	for (i = 0; i < NO; ++i)
+		accu[i] += H[i];
+	size += NI*SW;
+}
+
+static void last(const BYTE ba[NI*SW],size_t nb)
+{
+	int i,j,k,l;
+	BYTE temp[2*NI*SW];
+
+	size += nb;
+	size *= 8;
+	
+	i = 0;
+	while (i < nb)
+		temp[i++] = *ba++;
+	temp[i++] = 0x80u;
+	l = 1 + ((i + SS) > NI*SW);
+	k = l*NI*SW - SS;
+	while (i < k)
+		temp[i++] = 0u;
+#if BYTE_ORDER == BIG_ENDIAN
+	for (j = SS-1; j >= 0; --j)
+#elif BYTE_ORDER == LITTLE_ENDIAN
+	for (j = 0; j <= SS-1; ++j)
+#else
+	for (;;)
+#endif
+		temp[i++] = BA(&size)[j];
+	if (i != NI*SW*l)
+		abort();
+	next(temp);
+	if (l > 1)
+		next(temp+NI*SW);
+}
+
+static void pok(char fn[])
+{
+	print(accu,NO,fn);
+}
+
+int main(int ac,char *av[])
+{
+	int an;
+	FILE *ap;
+	size_t rv;
+	BYTE ar[NI*SW];
+
+	if (ac <= 1)
+		return fprintf(stderr,"usage: %s <filename> ...\n",av[0]),
+			EXIT_FAILURE;
+	for (an = 1; an < ac; ++an) {
+		ap = fopen(av[an],"rb");
+		if (!ap) {
+			perror(av[an]);
+			continue;
+		}
+		
+		init();
+		while ((rv = fread(ar,1,sizeof ar,ap)) == sizeof ar)
+			next(ar);
+		last(ar,rv);
+
+		if (ferror(ap))
+			perror("read()");
+		else
+			pok(av[an]);
+		if (fclose(ap))
+			perror("close()");
+	}
+	return EXIT_SUCCESS;
+}
